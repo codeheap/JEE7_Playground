585e1939c196667583514af11156ae9488bf53fd
codeheap <froorider@gmail.com>
2016-10-22 14:49:38 +0200

    Added Meals bean

--- pasadena/cheesecakefactory/cheesecakefactory_jeeservice/src/main/java/ch/froorider/cheesecakefactory_jeeservice/entity/Meal.java
+++ pasadena/cheesecakefactory/cheesecakefactory_jeeservice/src/main/java/ch/froorider/cheesecakefactory_jeeservice/entity/Meal.java
@@ -1,19 +1,50 @@
 package ch.froorider.cheesecakefactory_jeeservice.entity;
 
+import java.util.UUID;
+
 public class Meal{
 
 private String name;
 private String description;
 private int price;
-private uuid identifier;
+private UUID identifier;
 
-public Meal(){
-}
+    public Meal(String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        identifier = UUID.randomUUID();
+    }
 
-public Meal(name, description, price){
-    this.name = name;
-    this.description = description;
-    this.price = price;
-    this.identifier = new UUID();
-}
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public UUID getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(UUID identifier) {
+        this.identifier = identifier;
+    }
 }
