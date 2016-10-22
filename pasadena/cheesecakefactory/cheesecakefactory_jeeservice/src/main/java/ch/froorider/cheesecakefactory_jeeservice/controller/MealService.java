069054a2548bcf3a4265d1a0cb63995aab0d0b7c
codeheap <froorider@gmail.com>
2016-10-22 17:26:18 +0200

    added wrapper jar

---
+++ pasadena/cheesecakefactory/cheesecakefactory_jeeservice/src/main/java/ch/froorider/cheesecakefactory_jeeservice/controller/MealService.java
@@ -0,0 +1,12 @@
+package ch.froorider.cheesecakefactory_jeeservice.controller;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ * Created by Thomas on 22.10.2016.
+ */
+@SessionScoped
+@Named
+public class MealService {
+}
